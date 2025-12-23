8490c6893e51e9ba12b2f2b34a9472da7f103d96
Luis Grimaldi <lgrimaldi@smartautomatai.com>
2025-12-23 09:28:03 -0400

    v1 23/12

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+
+import { useEffect } from "react";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
+  useEffect(() => {
+    if (typeof window !== "undefined" && 'serviceWorker' in navigator && !__DEV__) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js');
+      });
+    }
+  }, []);
   return <Stack />;
 }
